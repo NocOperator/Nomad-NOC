bdfefb6552ab57c61b1ecb5d6d88e54cc36a801f
alyssacarroll <alyssa.carroll.22@cnu.edu>
2025-07-28 15:36:43 -0400

    reverted to NTS owned url

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 (() => {
   const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQohLpgMCkMfv1T6kUto-ole2NRCZCzfywnVNl98gxNFSY2BbFN4bVCpZu3QsL3DKLhwNqcqw8C8inf/pub?output=csv";
-  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbylYOYysTu6mD2nA3Hg-iSkjPpC_OcXmteBThhQ1fmt3HHWQPh_0FjT6pEvgZzUZXbDJw/exec";
+  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwEg3zDQTvzrkZPgnaIMRBT9InrP-Hz_yT7dOCK9KsXcOBPjnqqMjqtAjRG3f_zd3ae/exec";
 
   // ------- Utilities --------
   function formatDate(date = new Date()) {
