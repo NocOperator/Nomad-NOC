fbcc9f5e1ddfcf3480743ade9964cb5fa8c2f6d6
alyssacarroll <alyssa.carroll.22@cnu.edu>
2025-07-31 11:24:55 -0400

    10 box size

--- formTemplates.js
+++ formTemplates.js
@@ -184,7 +184,7 @@ const formTemplates = {
       <label class="switch-10"><input type="checkbox" name="SA14WAVE511MS"><span class="slider-10"></span></label>
     </div>
     <label>Comments:
-      <textarea name="Notes" rows="3" style="width:100%;"></textarea>
+      <textarea name="Notes" rows="1" style="width:100%;"></textarea>
     </label>
 </div>
 `,
